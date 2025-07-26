8f5739c2c125cbe91a93fa5bd9bda488d9567606
Paradox570Y1 <apuravgautam721@gmail.com>
2025-07-26 10:57:37 +0530

    Created dynamic table

---
+++ data.js
@@ -0,0 +1,22 @@
+const productListFromServer = [
+  {
+    name: "Product1",
+    price: "$10.00",
+    description: "This is a great product.",
+  },
+  {
+    name: "Product2",
+    price: "$13.00",
+    description: "This is a great product.",
+  },
+  {
+    name: "Product3",
+    price: "$8.00",
+    description: "This is a great product.",
+  },
+  {
+    name: "Product4",
+    price: "$4.00",
+    description: "This is a great product.",
+  },
+];
