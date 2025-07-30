074c20e047bac2f22a344997c5e904ad809e58c0
Paradox570Y1 <apuravgautam721@gmail.com>
2025-07-30 11:23:51 +0530

    changed structure of file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 8080;
-app.use(express.static('public'));
+app.use(express.static('frontend'));
 app.listen(port, function() {
-    console.log('Server is running on https://localhost:' + port);
+    console.log('Server is running on http://localhost:' + port);
 })
